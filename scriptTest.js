ba8bdf4fddf4cefb8da08759820619e60a9c1140
gennw <778855@mail.ru>
2023-03-29 12:17:09 +0300

    feat: sinxron

---
+++ scriptTest.js
@@ -0,0 +1,52 @@
+// let mone = ['Орел', 'Решка'];
+// function vy() {
+//   let choice = Math.random ();
+//   console.log(choice);
+//                   if (choice > 0.5) {
+//                     choice = 1; console.log(choice);
+//                   }
+//                     else { 
+//                       choice = 0;
+//                     }
+//        console.log(mone [choice]);   
+// }
+//    vy();   
+
+// console.log('Dragon' + ' stone');
+
+// let rublesPerDollar = 60;
+// let dollarsCount = 50 * 1.25; // 62.5
+// let rublesCount = dollarsCount * rublesPerDollar; // 3750
+
+// console.log('The price is ' + rublesCount + ' rubles');
+// console.log(`string text line 1
+// string text line 2`);
+// // BEGIN (write your solution here)
+// const eurosCount = 1000;
+// let euroDollarExchange = 1.06;
+// let dollarRubleExchange =  75.9;
+// console.log(`€${eurosCount} will be \$${eurosCount * euroDollarExchange}\nor \₽${eurosCount * euroDollarExchange * dollarRubleExchange}`)
+//     function sayHi (){
+//     console.log('Hi dear')
+// }
+// sayHi();
+// const magic = '\nyo\nu';
+// console.log(magic);
+
+// function compareValues(arr) {
+//   let firstValue = arr[0];
+//   let secondValue = arr[1];
+//   if (!isNaN(firstValue) || !isNaN(secondValue)) {
+//     return -1;
+//   } else if (firstValue === secondValue) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+//   console.log()
+// }
+
+let anyArr = [1, 3, 6, 10]
+let q = anyArr.push (11)
+console.log (q)
+// END
